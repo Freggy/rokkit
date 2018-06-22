67a91630bd8fee0366ace76e256a9b5c0f21da45
Yannic Rieger <riegeryannic@gmail.com>
2018-06-22 16:04:00 +0200

    Implement method for reading VarInts

---
+++ src/byte.rs
@@ -0,0 +1,35 @@
+use bytes::{BufMut};
+
+mod byte {
+
+}
+fn read_var_int(buf: &Buf) -> Result<i32, Error> {
+    let mut num_read = 0;
+    let mut result = 0;
+    let mut read: u8 = 0;
+    let mut cont = true;
+
+    while (read & 0b10000000) != 0 {
+        read = buf.get_u8();
+        let val = read & 0b01111111;
+        result |= val << (7 * num_read);
+        num_read += 1;
+
+        if num_read > 5 {
+            return Err("VarInt too big.")
+        }
+    }
+    Ok(result)
+}
+
+fn read_var_long(buf: &Buf) {
+
+}
+
+fn write_var_int(buf: &Buf) {
+
+}
+
+fn write_var_long(buf: &Buf) {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use tokio::net::TcpListener;
 fn main() {
     let addr = "127.0.0.1:1337".parse().unwrap();
     let listener = TcpListener::bind(&addr).expect("Unable to bind to address.");
-
+    /*
     let server = listener.incoming()
         .map_err(|e| eprintln!("accept failed = {:?}", e))
         .for_each(|sock| {
             let (reader, writer) = sock.split();
             // TODO: implement ServerListPing
-        });
+        }); */
 
-    Tokio::run(server);
+    //Tokio::run(server);
 }

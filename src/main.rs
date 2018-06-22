9cf3123c9878c2f5880dec0050edcc08d6a4a897
Yannic Rieger <riegeryannic@gmail.com>
2018-06-22 11:55:53 +0200

    Start work on server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate tokio;
+
+use tokio::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:1337".parse().unwrap();
+    let listener = TcpListener::bind(&addr).expect("Unable to bind to address.");
+
+    let server = listener.incoming()
+        .map_err(|e| eprintln!("accept failed = {:?}", e))
+        .for_each(|sock| {
+            let (reader, writer) = sock.split();
+            // TODO: implement ServerListPing
+        });
+
+    Tokio::run(server);
 }
